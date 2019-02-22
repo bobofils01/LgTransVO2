a44ad1db9ca375aa6498e0513357816dfd0569c6
romanskz <skubiszewski.roman@gmail.com>
2019-02-22 20:37:43 +0100

    Change date with null returned if paramter not well formatted

--- j--/tests/pass/Date.java
+++ j--/tests/pass/Date.java
@@ -1,16 +1,20 @@
 package pass;
 import java.lang.Integer;
+
 public class Date {
 
 	public static String[] MONTHS = new String[] { "January", "February", "March", "April", "May", "June", "July",
 			"August", "September", "October", "November", "December" };
 
 	public String date(String d) {
-		
-		// [0-9]{4}-[0-9]{2}-[0-9]{2}
 		int year = Integer.parseInt(d.substring(0, 4));
+		if(!(d.charAt(4) == '-'))
+			return null;
 		int monthNb = Integer.parseInt(d.substring(5, 7));
+		if(!(d.charAt(7) == '-'))
+			return null;
 		int day = Integer.parseInt(d.substring(8, 10));
+		
 		if (day <= 31 && day > 0 && monthNb <= 12 && monthNb > 0 && 0 <= year) {		
 			String month = MONTHS[monthNb - 1];
 	
